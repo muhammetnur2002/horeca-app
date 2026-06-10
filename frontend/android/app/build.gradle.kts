0b001a34fa6ee8e3fb8ab17a2536daa2fb11c7e5
Muhammetnur <nuryagdyyewmuhammetnur@gmail.com>
2026-06-10 11:11:46 +0300

    Set compileSdk to 36

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.horeca_app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
